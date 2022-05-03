9df885b7ab46beab2bf01721ac87f022f5f6680b
Victor <victor.goltsov@gmail.com>
2022-05-03 20:57:04 +0800

    300422

--- src/main/java/GoodNumber.java
+++ src/main/java/GoodNumber.java
@@ -1,3 +1,9 @@
+//7. Given an integer M perform the following conditional actions:
+//If M is multiple of 3 and 5 then return "Good Number".
+//If M is only multiple of 3 and not of 5 then return "Bad Number"
+//If M is only multiple of 5 and not of 3 then return "Poor Number"
+//If M doesn't satisfy any of the above conditions then return "-1"
+
 public class GoodNumber {
     public String GoodNumberAgirithm (int m) {
         if (m % 3 == 0 && m % 5 != 0) {
