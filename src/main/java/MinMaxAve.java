59b601290bfe4967d1f3f8698de23ac47311c050
Victor <victor.goltsov@gmail.com>
2022-05-05 00:38:36 +0800

    300422

--- src/main/java/MinMaxAve.java
+++ src/main/java/MinMaxAve.java
@@ -1,23 +1,31 @@
+import static Utils.Utils.*;
+
 public class MinMaxAve {
 
     public double[] MinMaxAveAlgorithm (int [] arr, int ind1, int ind2) {
+        checkzero(arr);
+        if (arr.length < (Math.abs(ind2 - ind1) + 1) || Math.min(ind2, ind1) < 0 || Math.max(ind2, ind1) > (arr.length - 1)) {
 
-        double min = Integer.MAX_VALUE;
-        double max = Integer.MIN_VALUE;
-        double avg = 0;
-        for (int i = ind1; i <= ind2; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            } if (arr[i] > max) {
-                max = arr[i];
+            return new double[]{};
+        } else {
+            double min = Integer.MAX_VALUE;
+            double max = Integer.MIN_VALUE;
+            double avg = 0;
+            for (int i = ind1; i <= ind2; i++) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                avg += (double) arr[i] / (ind2 - ind1 + 1);
             }
-            avg = avg + arr[i];
+            double[] arrMminMaxAve = new double[3];
+            arrMminMaxAve[0] = min;
+            arrMminMaxAve[1] = max;
+            arrMminMaxAve[2] = avg;
+
+            return arrMminMaxAve;
         }
-        avg = avg / (ind2 -ind1 +1);
-        double [] arrMminMaxAve = new double[3];
-        arrMminMaxAve[0] = min;
-        arrMminMaxAve[1] = max;
-        arrMminMaxAve[2] = avg;
-        return arrMminMaxAve;
     }
 }
--- src/test/java/MinMaxAveTest.java
+++ src/test/java/MinMaxAveTest.java
@@ -3,15 +3,81 @@ import org.junit.jupiter.api.Test;
 
 public class MinMaxAveTest {
     @Test
-    public void testMinMaxAve () {
+    public void testMinMaxAveHP () {
        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int ind1 = 2;
         int ind2 = 6;
-        double[] expectedResult = {3,7,5};
+        double[] expectedResult = {3, 7, 5};
 
         MinMaxAve obj = new MinMaxAve();
         double [] actualResult = obj.MinMaxAveAlgorithm(arr, ind1, ind2);
 
         Assertions.assertArrayEquals(expectedResult, actualResult);
     }
+
+    @Test
+    public void testMinMaxAveHP2MinLanth () {
+        int[] arr = {1};
+        int ind1 = 0;
+        int ind2 = 0;
+        double[] expectedResult = {1, 1, 1};
+
+        MinMaxAve obj = new MinMaxAve();
+        double [] actualResult = obj.MinMaxAveAlgorithm(arr, ind1, ind2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testMinMaxAveIndexAtTheBoundary () {
+        int[] arr = {2, 3, 4};
+        int ind1 = 0;
+        int ind2 = 2;
+        double[] expectedResult = {2, 4, 3};
+
+        MinMaxAve obj = new MinMaxAve();
+        double [] actualResult = obj.MinMaxAveAlgorithm(arr, ind1, ind2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testMinMaxAveIndexesOutOfArr () {
+        int[] arr = {2, 3, 4};
+        int ind1 = 0;
+        int ind2 = 3;
+        double[] expectedResult = {};
+
+        MinMaxAve obj = new MinMaxAve();
+        double [] actualResult = obj.MinMaxAveAlgorithm(arr, ind1, ind2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testMinMaxAveIndexesOutOfArr2 () {
+        int[] arr = {2, 3, 4};
+        int ind1 = -1;
+        int ind2 = 2;
+        double[] expectedResult = {};
+
+        MinMaxAve obj = new MinMaxAve();
+        double [] actualResult = obj.MinMaxAveAlgorithm(arr, ind1, ind2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testMinMaxAveIndexesOutOfArr5 () {
+        int[] arr = {2, 3, 4, 5};
+        int ind1 = 0;
+        int ind2 = 4;
+        double[] expectedResult = {};
+
+        MinMaxAve obj = new MinMaxAve();
+        double [] actualResult = obj.MinMaxAveAlgorithm(arr, ind1, ind2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
 }
